a0940eb1eae32c25263285ce7e9e841d43605525
Jake Wagner <jkwagneriii@gmail.com>
2020-11-30 13:18:35 -0600

    importing profile component to blog page, now working on redirecting after authentication

--- client/src/pages/Blog.js
+++ client/src/pages/Blog.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import './Blog.css'
+import Profile from '../components/Profile';
 
 function Blog() {
     return (
         <div className="blog-body">
             <Header />
             HELLO SHEEP
+            <Profile />
         </div>
     )
 }
