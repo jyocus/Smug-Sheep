7e743d397ec4d34ca88396569b2f7dc3fcc016a7
Justin Yocus <justin.o.yocus@gmail.com>
2020-12-01 11:46:12 -0600

    testing mongo connection and routes

--- controllers/genericController.js
+++ controllers/genericController.js
@@ -1,10 +1,39 @@
 //  Connect to db
 // const db = require('../models');
 const db = 'hello';
-
+const blogModel = require('../models')
 // Export Controller Methods
+// module.exports = {
+//     genericTest: function(req, res) {
+//         res.json(db);
+//     }
+// };
+
 module.exports = {
-    genericTest: function(req, res) {
-        res.json(db);
+    findAll: function(req, res) {
+      blogModel.Post.find(req.query)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    // findById: function(req, res) {
+    //   db.Post.findById(req.params.id)
+    //     .then(dbModel => res.json(dbModel))
+    //     .catch(err => res.status(422).json(err));
+    // },
+    create: function(req, res) {
+        blogModel.Post.create(req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    update: function(req, res) {
+        blogModel.Post.findOneAndUpdate({ _id: req.params.id }, req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
     }
-};
+    // remove: function(req, res) {
+    //   db.Post.findById({ _id: req.params.id })
+    //     .then(dbModel => dbModel.remove())
+    //     .then(dbModel => res.json(dbModel))
+    //     .catch(err => res.status(422).json(err));
+    // }
+  };
