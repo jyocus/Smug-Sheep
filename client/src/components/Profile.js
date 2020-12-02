8c05751b0989e7d69eb9ac1d45e3b0b275462513
Jake Wagner <jkwagneriii@gmail.com>
2020-12-02 14:05:49 -0600

    woah woah profile card is working

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -7,35 +7,110 @@ import JSONPretty from "react-json-pretty";
 import Test from "./Test";
 import API from "../utils/API";
 import LogOutButton from "./LogOutButton"
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Avatar } from '@material-ui/core';
 
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 300,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      display: 'block', 
+      marginTop: '10px',
+      opacity: 0.75
+    //   background: 'linear-gradient(45deg, #FF8E53 30%, #FE6B8B 90%)'
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+    button: {
+        textAlign: 'center',
+        marginBottom: '5px'
+    },
+    avatar: {
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      display: 'block',
+      opacity: 1
+    }
+});
 
-export const Profile = () => {
-
-    //the isAuthenticated setState(?) property now it will only be rendered if user is authenticated. Need some JSX at the Bottom.
-const { user, isAuthenticated } = useAuth0()
-
-
-
-API.getUser(user)
-
+export default function Profile() {
+    const classes = useStyles();
+    const { user, isAuthenticated } = useAuth0()
+    API.getUser(user)
+    // const bull = <span className={classes.bullet}>•</span>;
+  
     return (
         isAuthenticated && (
-    <div>
-        <JSONPretty data={ user }/>
-        <a href="/blog">blog</a>
-        <LogOutButton />
-        
-    {/* <img src={user.picture} alt={user.name} />
-        <h1>{user.name}</h1>
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography className={classes.title} color="textPrimary" gutterBottom>
+            {user.name}
+          </Typography>
+          <Typography className={classes.title} color="textPrimary">
+            Email: {user.email}
+          </Typography>
 
-        <p>Reach out to me about my baaaaogs at: {user.email} </p>
-            
-
-        <footer>Is my Email Verified: {user.email_verified} </footer>             */}
-    </div>
-    )
-)
-
+          <Avatar className={classes.avatar} alt={user.name} src={user.picture} />
+          {/* <Typography className={classes.pos} color="textSecondary">
+            adjective
+          </Typography> */}
+          {/* <Typography variant="body2" component="p">
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </Typography> */}
+        </CardContent>
+        {/* <CardActions> */}
+          <Button href="/blog" variant="contained" className={classes.button} color="secondary" size="small">Start Shearing</Button>
+        {/* </CardActions> */}
+      </Card>
+        )
+    );
 }
 
-export default Profile
+
+// export const Profile = () => {
+
+//     //the isAuthenticated setState(?) property now it will only be rendered if user is authenticated. Need some JSX at the Bottom.
+// const { user, isAuthenticated } = useAuth0()
+
+
+
+// API.getUser(user)
+
+//     return (
+//         isAuthenticated && (
+//             <div>
+//             {/* <JSONPretty data={ user }/> */}
+//             <a href="/blog">blog</a>
+//             <LogOutButton />
+            
+//         <img src={user.picture} alt={user.name} />
+//             <h1>{user.name}</h1>
+    
+//             <p>Reach out to me about my baaaaogs at: {user.email} </p>
+                
+    
+//             <footer>Is my Email Verified: {user.email_verified} </footer>            
+//         </div>
+//     )
+// )
+
+// }
+
+// export default Profile
