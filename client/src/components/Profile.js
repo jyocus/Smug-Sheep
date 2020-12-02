73d80c5913f4f1e127e2f3cbb0feb32a3b249d73
Jake Wagner <jkwagneriii@gmail.com>
2020-12-02 14:11:55 -0600

    got rid of unused imports

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,15 +1,9 @@
 //This is a small example of what we can do with Auth0 from: https://www.youtube.com/watch?v=MqczHS3Z2bc
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react';
-//Optional npm package to make stringified objects 'prettier'
-import JSONPretty from "react-json-pretty";
-    // ---->   <JSONPretty data={ user }/>
-import Test from "./Test";
 import API from "../utils/API";
-import LogOutButton from "./LogOutButton"
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
@@ -23,7 +17,6 @@ const useStyles = makeStyles({
       display: 'block', 
       marginTop: '10px',
       opacity: 0.75
-    //   background: 'linear-gradient(45deg, #FF8E53 30%, #FE6B8B 90%)'
     },
     bullet: {
       display: 'inline-block',
@@ -52,7 +45,6 @@ export default function Profile() {
     const classes = useStyles();
     const { user, isAuthenticated } = useAuth0()
     API.getUser(user)
-    // const bull = <span className={classes.bullet}>•</span>;
   
     return (
         isAuthenticated && (
@@ -71,35 +63,3 @@ export default function Profile() {
         )
     );
 }
-
-
-// export const Profile = () => {
-
-//     //the isAuthenticated setState(?) property now it will only be rendered if user is authenticated. Need some JSX at the Bottom.
-// const { user, isAuthenticated } = useAuth0()
-
-
-
-// API.getUser(user)
-
-//     return (
-//         isAuthenticated && (
-//             <div>
-//             {/* <JSONPretty data={ user }/> */}
-//             <a href="/blog">blog</a>
-//             <LogOutButton />
-            
-//         <img src={user.picture} alt={user.name} />
-//             <h1>{user.name}</h1>
-    
-//             <p>Reach out to me about my baaaaogs at: {user.email} </p>
-                
-    
-//             <footer>Is my Email Verified: {user.email_verified} </footer>            
-//         </div>
-//     )
-// )
-
-// }
-
-// export default Profile
