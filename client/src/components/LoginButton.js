60e3e252c3074a88e6c1b4a15c511f01e3aed9c4
Jake Wagner <jkwagneriii@gmail.com>
2020-12-06 13:54:05 -0600

    cleaned up loginbutton.js

--- client/src/components/LoginButton.js
+++ client/src/components/LoginButton.js
@@ -1,11 +1,10 @@
-//With ES7 React tool in VSStore. type 'rafce' and it will generate a boilerplate of a ComponentName.js. 
+// With ES7 React tool in VSStore - type 'rafce' and it will generate a boilerplate of a ComponentName.js. 
 import React from 'react'
-//Importing the Hook makeStyles from @material
+// Material UI
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-//this is a Hook made by Auth0
+// Auth0
 import { useAuth0 } from '@auth0/auth0-react'
-import API from '../utils/API';
 
 const useStyles = makeStyles(theme => ({
     '@global': {
@@ -13,22 +12,13 @@ const useStyles = makeStyles(theme => ({
             margin: theme.spacing(3, 0, 2)
         }
       }
-}))
-
+}));
 
 const LoginButton = () => {
     const classes = useStyles
-
-   
-
     //Destructuring the Hook 
-    const {loginWithRedirect} = useAuth0();
-
-    // console.log(loginWithRedirect);
-
-
+    const { loginWithRedirect } = useAuth0();
     return (
-        // Creating a button that has an onClick function. With JSX logic we can call the const above into an OnClick function
         <Button onClick={() => loginWithRedirect()}
             type="submit"
             fullWidth
@@ -38,6 +28,6 @@ const LoginButton = () => {
             Join the Flock!
         </Button>
     )
-}
+};
 
 export default LoginButton
