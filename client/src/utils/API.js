97bc5f08dc91b1cd7fa04570fa0e79601e2d8da9
Jake Wagner <jkwagneriii@gmail.com>
2020-12-06 14:14:12 -0600

    cleaned up API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,37 +4,25 @@ let userData = {}
 
 export default {
 
-getUser: function(data) {
+  getUser: function (data) {
     //return axios.get("/api/user/:id")
     console.log(data);
     userData = data;
     return userData;
-},
-showUser: function() {
+  },
+  showUser: function () {
     console.log(userData)
-},
-
+  },
   // Gets all posts
-  getPosts: function(res) {
+  getPosts: function (res) {
     return axios.get("/api/posts");
   },
-
   // Saves a post to the database
-  savePost: function(postData) {
+  savePost: function (postData) {
     console.log(postData)
     return axios.post("/api/posts", postData);
   },
-
-  getQuote: function(){
+  getQuote: function () {
     return axios.get("https://cors-anywhere.herokuapp.com/https://zenquotes.io/api/random")
   }
-
-    // // Deletes the post with the given id
-    // deletePost: function(id) {
-    //     return axios.delete("/api/posts/" + id);
-    //   },
-
-
-
-
-}
+};
