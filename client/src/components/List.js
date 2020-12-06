ee620588e4efa99d3ee90421ccd62423f3cd92b1
Jake Wagner <jkwagneriii@gmail.com>
2020-12-06 13:51:11 -0600

    cleaned up list.js

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FixedSizeList } from 'react-window';
+// Material UI
 import { makeStyles } from '@material-ui/core/styles';
 import ListItem from '@material-ui/core/ListItem';
 import Typography from '@material-ui/core/Typography';
 import ListItemText from '@material-ui/core/ListItemText';
-import { FixedSizeList } from 'react-window';
 import Grid from '@material-ui/core/Grid';
-import Divider from '@material-ui/core/Divider';
-import Bleetify from "bleetify"
-
 //Bleetify!
+import Bleetify from "bleetify"
 const blogText = " THE FLOCK'S BLEETS"
 const bleet = Bleetify.bleet(blogText, 75);
 
@@ -17,15 +16,12 @@ const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
     height: 350,
-    // maxWidth: 500,
     backgroundColor: theme.palette.background.paper,
-    // marginRight: "50px",
     borderRadius: '10px',
     paddingRight: "10px",
     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
   },
   header: {
-    // fontWeight: 'bold',
     paddingTop: '10px',
     paddingBottom: '10px',
     fontFamily: 'impact',
@@ -40,21 +36,21 @@ function renderRow(props) {
   return (
     <Grid>
       <ListItem button style={style} key={index}>
+        {/* LIST TITLE  */}
         <Grid item md={2}>
-        {/* <ListItemText  primary={`${item.title}`} /> */}
-        <ListItemText
-        disableTypography
-        primary={<Typography type="body2" style={{ color: '#FFFFFF', fontWeight: '900' }}>{`${item.title}`}</Typography>}
-      />
+          <ListItemText
+            disableTypography
+            primary={<Typography type="body2" style={{ color: '#FFFFFF', fontWeight: '900' }}>{`${item.title}`}</Typography>}
+          />
         </Grid>
+        {/* LIST BODY  */}
         <Grid item md={10}>
-        <ListItemText primary={`${item.body}`} />
+          <ListItemText primary={`${item.body}`} />
         </Grid>
       </ListItem>
     </Grid>
-    
   );
-}
+};
 
 renderRow.propTypes = {
   index: PropTypes.number.isRequired,
@@ -62,13 +58,11 @@ renderRow.propTypes = {
 };
 
 export default function VirtualizedList(props) {
-
   const getBleet = bleet;
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <header className={classes.header}>{getBleet}</header>
-      
       <FixedSizeList itemData={props.posts} height={300} width={"100%"} itemSize={120} itemCount={props.posts.length}>
         {renderRow}
       </FixedSizeList>
