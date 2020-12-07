7980ed2697d4e7d19f824eefa826ab757d0ea299
Alex Eesley <alexandra.eesley@gmail.com>
2020-12-07 10:57:35 -0600

    fixing one more compiling error

--- client/src/index.js
+++ client/src/index.js
@@ -22,7 +22,7 @@ ReactDOM.render(
 
 
   //We might want StrictMode at some point, might need to look more into how it handles bad lifecylce components,etc. 
-{/* <React.StrictMode></React.StrictMode> */}
+/* <React.StrictMode></React.StrictMode> */
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
